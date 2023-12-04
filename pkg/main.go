a060019b48416d033c5b4415dcbfe2cbf1ab850e
Andy Frank <afrankvt@gmail.com>
2023-12-04 16:53:31 -0500

    misc fixes

--- pkg/main.go
+++ pkg/main.go
@@ -25,7 +25,7 @@ func main() {
 	// from Grafana to create different instances of SampleDatasource (per datasource
 	// ID). When datasource configuration changed Dispose method will be called and
 	// new datasource instance created using NewSampleDatasource factory.
-	if err := datasource.Manage("novant-novant-datasource", plugin.NewDatasource, datasource.ManageOpts{}); err != nil {
+	if err := datasource.Manage("novant-datasource", plugin.NewDatasource, datasource.ManageOpts{}); err != nil {
 		log.DefaultLogger.Error(err.Error())
 		os.Exit(1)
 	}
